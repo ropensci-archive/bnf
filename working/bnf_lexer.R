93fdb49e50595bc4a7f0070a0edef790c5de9b49
mike <mikefc@coolbutuseless.com>
2019-12-13 09:59:28 +1000

    expanding lexer

--- working/bnf_lexer.R
+++ working/bnf_lexer.R
@@ -1,22 +1,26 @@
 
 
 library(stringr)
-source('lexer.R')
+source(here::here('working', 'lexer.R'))
 
-# Expr ::= Term ('+' Term | '-' Term)*
-# Term ::= Number ('*' Number | '/' Number)*
+# Expr   ::= Term ('+' Term | '-' Term)*
+# Term   ::= Number ('*' Number | '/' Number)*
 # Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+
 
-line1 <- "Expr ::= Term ('+' Term | '-' Term)*"
+line0 <- "dummy ::= a b c ;"
+line1 <- "dummy ::= (a | b | c)+ ;"
+
+
+line10 <- "Expr ::= Term ('+' Term | '-' Term)*"
 
 simple_bnf <- "
-Expr ::= Term ('+' Term | '-' Term)* ;
-Term ::= Number ('*' Number | '/' Number)* ;
+Expr   ::= Term ('+' Term | '-' Term)* ;
+Term   ::= Number ('*' Number | '/' Number)* ;
 Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+ ;"
 
 simple_bnf_with_factor <- "
-Expr ::= Term ('+' Term | '-' Term)* ;
-Term ::= Factor ('*' Factor | '/' Factor)* ;
+Expr   ::= Term ('+' Term | '-' Term)* ;
+Term   ::= Factor ('*' Factor | '/' Factor)* ;
 Factor ::= ['-'] (Number | '(' Expr ')') ;
 Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+ ;
 "
@@ -39,7 +43,7 @@ Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+ ;
 #      treated differently, but for now i just look up all words to
 #      see if they exist in the global spec
 
-patterns <- c(
+bnf_patterns <- c(
   ASSIGN      = "::=",
   ENDOFRULE   = "\\s+;",
   OR          = "\\|",
@@ -56,14 +60,53 @@ patterns <- c(
 )
 
 
-tokens <- lex(simple_bnf, patterns = patterns)
-tokens[names(tokens) != 'whitespace']
+tokens <- lex(line0, patterns = bnf_patterns)
+tokens <- tokens[names(tokens) != 'whitespace']
+tokens
+
+peek_type     <- function(tokens) {     names(tokens[ 1]) }
+peek_value    <- function(tokens) {    unname(tokens[ 1]) }
+consume_token <- function(tokens) { invisible(tokens[-1])  }
+assert_type   <- function(type, tokens) {
+  stopifnot(identical(peek_type(tokens), type))
+}
+end_of_tokens <- function(tokens) {length(tokens) == 0}
 
 
+`%notin%` <- Negate(`%in%`)
 
+# Parse rule
+tokens
+assert_type('WORD', tokens)
+rule_name <- peek_value(tokens)
+tokens <- consume_token(tokens)
+assert_type('ASSIGN', tokens)
+tokens
+tokens <- consume_token(tokens)
+tokens
 
+# parse items
+item_delimiters <- c(
+  'ENDOFRULE',
+  'OR',
+  'LBRACKET',
+  'RBRACKET',
+  'RBRACKET1P',
+  'RBRACKET0P',
+  'SLBRACKET',
+  'SRBRACKET'
+)
 
+items <- list()
+while (peek_type(tokens) %notin% item_delimiters) {
+  items  <- append(items, peek_value(tokens))
+  tokens <- consume_token(tokens)
+}
 
+N    <- 'one'
+type <- 'all'
 
+rule <- setNames(list(list(items = items, N = N, type = type)), rule_name)
+cat(deparse(rule))
 
 
