a97c8f2e3b19c25c616ad1733377021d7a5b7abe
Alex Whan <alexwhan@gmail.com>
2019-12-12 17:02:48 +1100

    ooh pretty

--- simple.R
+++ simple.R
@@ -21,11 +21,22 @@ simple <- list(
     list(
       items = list(
         list(items = list('*', 'number'), N = 'one'),
-        list(items = list('/', 'number'), N = 'one')
+        list(items = list('/', 'number'), N = 'one'),
+        list(items = list('*', 'func'), N = 'one'),
+        list(items = list('/', 'func'), N = 'one'),
+        list(items = list('*', 'var'), N = 'one'),
+        list(items = list('/', 'var'), N = 'one')
       ),
       type = 'choice',
       N    = 'zero_or_more'
     )
+  ),
+  func = list(
+    list(items = list('cos', 'sin'), N = 'one', type = 'choice'),
+    list(items = list('(', 'term', ')'), N = 'one')
+  ),
+  var = list(
+    items = list('x', 'y'), N = 'one', type = 'choice'
   ),
   number = list(
     items = as.list(as.character(0:9)),
@@ -41,6 +52,7 @@ library(purrr)
 global_spec <- simple
 
 create <- function(spec) {
+  # browser()
   if (is.character(spec)) {
     if (spec %in% names(global_spec)) {
       return(create(global_spec[[spec]]))
@@ -62,7 +74,7 @@ create <- function(spec) {
     type <- spec$type %||% 'all'
 
     if (type == 'choice') {
-      items <- sample(spec$items, N)
+      items <- sample(spec$items, N, replace = TRUE)
     } else {
       items <- spec$items
     }
@@ -82,18 +94,35 @@ create <- function(spec) {
 
 spec <- simple$expr
 
+x <- 3
+y <- 2
 zz <- create(simple$expr)
 zz
-eval(parse(text = zz))
+
+fun <- function(x, y) {
+  eval(parse(text = zz))
+}
 
 
+library(tidyverse)
+n <- 100
 
+aargh <- function(n) {
+  zz <- create(simple$expr)
 
+  fun <- function(x, y) {
+    eval(parse(text = zz))
+  }
 
+  p <- expand.grid(1:n, 1:n) %>%
+    as_tibble %>%
+    mutate(z = map2_dbl(Var1, Var2, fun)) %>%
+    ggplot(aes(Var1, Var2)) +
+    geom_tile(aes(fill = log(z))) +
+    scale_fill_viridis_c(guide = "none") +
+    theme_void()
+  print(p)
+}
 
-
-
-
-
-
+aargh(50)
 
