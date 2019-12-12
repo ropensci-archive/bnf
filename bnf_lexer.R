6a91f4b24e0686a01caaccbd624d26569c7375c2
mike <mikefc@coolbutuseless.com>
2019-12-13 08:57:44 +1000

    tweaking lexer

--- bnf_lexer.R
+++ bnf_lexer.R
@@ -5,20 +5,20 @@ source('lexer.R')
 
 # Expr ::= Term ('+' Term | '-' Term)*
 # Term ::= Number ('*' Number | '/' Number)*
-# Number ::= Digit+
+# Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+
 
 line1 <- "Expr ::= Term ('+' Term | '-' Term)*"
 
 simple_bnf <- "
 Expr ::= Term ('+' Term | '-' Term)* ;
 Term ::= Number ('*' Number | '/' Number)* ;
-Number ::= Digit+ ;"
+Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+ ;"
 
 simple_bnf_with_factor <- "
 Expr ::= Term ('+' Term | '-' Term)* ;
 Term ::= Factor ('*' Factor | '/' Factor)* ;
 Factor ::= ['-'] (Number | '(' Expr ')') ;
-Number ::= Digit+ ;
+Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+ ;
 "
 
 simple_bnf_with_functions <- "
@@ -26,9 +26,18 @@ Expr   ::= Term ('+' Term | '-' Term)* ;
 Term   ::= Factor ('*' Factor | '/' Factor)* ;
 Factor ::= ['-'] (Number | '(' Expr ')' | Call) ;
 Call   ::= ('cos(' Expr ')' | 'sin(' Expr ')') ;
-Number ::= Digit+ ;
+Number ::= ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+ ;
 "
 
+
+# Notes/Restrictions
+# '+' and '*' modifiers are only allowed on bracketed expressions
+# '[]' delimits an optional item i.e. N = 'zero_or_one'
+# '()' delimits a choice
+# QWORD is a quoted word - only single quotes allowed
+# WORD is a bare word. Eventually WORD and QWORD should be
+#      treated differently, but for now i just look up all words to
+#      see if they exist in the global spec
 
 patterns <- c(
   ASSIGN      = "::=",
@@ -42,14 +51,19 @@ patterns <- c(
   SRBRACKET   = '\\]',
   number      = pattern_number,
   QWORD       = "'.*?'",
-  WORD0P      = "\\w+\\*",
-  WORD1P      = "\\w+\\+",
   WORD        = "\\w+",
   whitespace  = "\\s+"
 )
 
 
-tokens <- lex(simple_bnf_with_functions, patterns = patterns)
+tokens <- lex(simple_bnf, patterns = patterns)
 tokens[names(tokens) != 'whitespace']
+
+
+
+
+
+
+
 
 
