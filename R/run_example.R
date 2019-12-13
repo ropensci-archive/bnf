50e362e719db49f4670b803c889b253eeb36fd39
Alex Whan <alexwhan@gmail.com>
2019-12-13 15:31:05 +1100

    run_example

---
+++ R/run_example.R
@@ -0,0 +1,17 @@
+#' run_example
+#'
+#' @return
+#' @export
+#'
+#' @examples
+run_example <- function() {
+  if(!requireNamespace("shiny", quietly = TRUE)) {
+    stop("The shiny package is not available. Install with `install.packages('shiny')`.")
+  }
+  appDir <- system.file("shiny", "bnf", package = "bnf")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `bnf`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
